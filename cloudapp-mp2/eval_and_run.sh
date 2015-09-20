cfad8f4c376068c9afa8874fff51bb113a1e9da8
au9ustine <duke.augustine@gmail.com>
2015-09-20 23:52:19 +0800

    add TopTitles based on WP2 tutorial

--- cloudapp-mp2/eval_and_run.sh
+++ cloudapp-mp2/eval_and_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-target=TitleCount
+# target=TitleCount
+target=TopTitles
 
 # Compile
 hadoop com.sun.tools.javac.Main "$target".java -d build
@@ -9,11 +10,16 @@ hadoop com.sun.tools.javac.Main "$target".java -d build
 jar -cvf "$target".jar -C build/ ./
 
 # Clean
-hadoop fs -rm -r /mp2/A-output
+# hadoop fs -rm -r /mp2/A-output
+hadoop fs -rm -r /mp2/B-output
 
-# Run
-hadoop jar "$target".jar "$target" -D stopwords=/mp2/misc/stopwords.txt -D delimiters=/mp2/misc/delimiters.txt /mp2/titles /mp2/A-output
+# Run TitleCount
+# hadoop jar "$target".jar "$target" -D stopwords=/mp2/misc/stopwords.txt -D delimiters=/mp2/misc/delimiters.txt /mp2/titles /mp2/A-output
+
+# Run TopTitles
+hadoop jar "$target".jar "$target" -D stopwords=/mp2/misc/stopwords.txt -D delimiters=/mp2/misc/delimiters.txt -D N=5 /mp2/titles /mp2/B-output
 
 # Check result
-hadoop fs -cat /mp2/A-output/part* | sort -n -k2 -r | head -10
+# hadoop fs -cat /mp2/A-output/part* | sort -n -k2 -r | head -10
+hadoop fs -cat /mp2/B-output/part* | head -10
 
