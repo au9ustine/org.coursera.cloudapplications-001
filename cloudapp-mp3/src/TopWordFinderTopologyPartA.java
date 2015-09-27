570f45ef3befb429fbdd9db200c26812328280a8
au9ustine <duke.augustine@gmail.com>
2015-09-28 02:31:29 +0800

    Week_3 - TopWordFinderTopologyPartA

--- cloudapp-mp3/src/TopWordFinderTopologyPartA.java
+++ cloudapp-mp3/src/TopWordFinderTopologyPartA.java
@@ -37,15 +37,27 @@ public class TopWordFinderTopologyPartA {
 
     ------------------------------------------------- */
 
+    builder.setSpout("spout", new RandomSentenceSpout(), 5);
+    builder.setBolt("split", new SplitSentenceBolt(), 8).shuffleGrouping("spout");
+    builder.setBolt("count", new WordCountBolt(), 12).fieldsGrouping("split", new Fields("word"));
 
-    config.setMaxTaskParallelism(3);
+    if (args != null && args.length > 0) {
+        config.setNumWorkers(3);
+        StormSubmitter.submitTopology(args[0], config, builder.createTopology());
+    } else {
+        config.setMaxTaskParallelism(3);
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("word-count", config, builder.createTopology());
 
-    LocalCluster cluster = new LocalCluster();
-    cluster.submitTopology("word-count", config, builder.createTopology());
+        //wait for 60 seconds and then kill the topology
+        Thread.sleep(60 * 1000);
 
-    //wait for 60 seconds and then kill the topology
-    Thread.sleep(60 * 1000);
+        cluster.shutdown();
+    }
 
-    cluster.shutdown();
+
+
+
+
   }
 }
